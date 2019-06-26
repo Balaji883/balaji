ad36784301e69f71c3a208336828a7d114977076
Balaji <Balaji@LAPTOP-2EUHLU59>
2019-06-26 10:28:36 +0530

    eg5

---
+++ fundamentals/src/fundamentals/fifth.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class fifth {
+			public static void main(String[] args) {
+				char s='p';
+				if((s>='A' && s<='Z')||(s>='a' && s<='z'))
+					System.out.println("alphabet");
+				else if(s>='0' && s<='9')
+				    System.out.println("digit");
+				else
+					System.out.println("spl character");
+				
+
+			}
+
+		}
+
